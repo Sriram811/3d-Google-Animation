3abb3453bdc5cfcb0d6b3bbf0034021a74db3a8e
SRIRAM T <144657893+Sriram811@users.noreply.github.com>
2025-08-13 14:22:00 +0530

    Add files via upload

---
+++ g.js
@@ -0,0 +1,55 @@
+let g = document.getElementById("g1");
+let o = document.getElementById("o2");
+let o11 = document.getElementById("oo2");
+let got = document.getElementById("hlo");
+let l = document.getElementById("l1");
+let e = document.getElementById("e1");
+
+
+g.addEventListener("mouseover", function () {
+  g.style.textShadow = "6px 5px 5px lightblue";
+});
+
+o.addEventListener("mouseover", function () {
+  o.style.textShadow = "6px 5px 5px lightpink";
+});
+
+o11.addEventListener("mouseover", function () {
+  o11.style.textShadow = "6px 5px 5px lightyellow";
+});
+
+got.addEventListener("mouseover", function () {
+  got.style.textShadow = "6px 5px 5px lightblue";
+});
+
+l.addEventListener("mouseover", function () {
+  l.style.textShadow = "6px 5px 5px lightgreen";
+});
+
+e.addEventListener("mouseover", function () {
+  e.style.textShadow = "6px 5px 5px lightpink";
+});
+
+// g.addEventListener("mouseout", function () {
+//   g.style.textShadow="0px 0px 0px blue";
+// });
+
+// o.addEventListener("mouseout", function () {
+//   o.style.textShadow = "0px 0px 0px red";
+// });
+
+// o11.addEventListener("mouseout", function () {
+//   o11.style.textShadow = "0px 0px 0px yellow";
+// });
+
+// got.addEventListener("mouseout", function () {
+//   got.style.textShadow = "0px 0px 0px blue";
+// });
+
+// l.addEventListener("mouseout", function () {
+//   l.style.textShadow = "0px 0px 0px green";
+// });
+
+// e.addEventListener("mouseout", function () {
+//   e.style.textShadow = "0px 0px 0px red";
+// });
